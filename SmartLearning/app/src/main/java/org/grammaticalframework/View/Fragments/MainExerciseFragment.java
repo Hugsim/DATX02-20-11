d6c15cf01368dd7676f6f5acb244fb524c45e32e
augustas <augustas@student.chalmers.se>
2020-02-20 18:37:15 +0100

    replace navigation via activities with fragments
    This commit will remove all but one activity and make the application fragment-reliant, all pages will be a fragment on the same activity

---
+++ SmartLearning/app/src/main/java/org/grammaticalframework/View/Fragments/MainExerciseFragment.java
@@ -0,0 +1,23 @@
+package org.grammaticalframework.View.Fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import org.grammaticalframework.R;
+
+public class MainExerciseFragment extends BaseFragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.activity_exercises, container, false);
+        return v;
+
+    }
+}
---
+++ SmartLearning/app/src/main/java/org/grammaticalframework/View/Fragments/MainHomeFragment.java
@@ -0,0 +1,22 @@
+package org.grammaticalframework.View.Fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.grammaticalframework.R;
+
+public class MainHomeFragment extends BaseFragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_main_home, container, false);
+        return v;
+
+    }
+}
