986271ee7cf1575fa7e942d04e1fd30a78d0131b
hagerg <42995901+hagerg@users.noreply.github.com>
2020-02-27 17:20:27 +0100

    Now prints the different correct inflections of the first word in the given sentence

--- SmartLearning/app/src/main/java/org/grammaticalframework/View/MainActivity.java
+++ SmartLearning/app/src/main/java/org/grammaticalframework/View/MainActivity.java
@@ -11,6 +11,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.util.Map;
+
 public class MainActivity extends BaseActivity {
     private MainViewModel viewModel;
     private static final String TAG = MainActivity.class.getSimpleName();
@@ -28,11 +30,11 @@ public class MainActivity extends BaseActivity {
         swe = mSmartLearning.getTargetConcr();
 
         Expr e = Expr.readExpr("PhrUtt NoPConj (UttS (UseCl (TTAnt TPast ASimul) PPos (PredVP (UsePron we_Pron) (AdvVP (UseV eat_2_V) a_la_carte_Adv)))) NoVoc");
-        removeFirstVerb(e);
-        Object[] bs = swe.bracketedLinearize(e);
-        //printChildren(bs[0]);
+        //removeFirstVerb(e);
+        Object[] bs = eng.bracketedLinearize(e);
+        printChildren(bs[0]);
 
-        wordTranslator("nail");
+        //wordTranslator("nail");
 
         startActivity(intent);
         finish();
@@ -40,14 +42,16 @@ public class MainActivity extends BaseActivity {
 
     private void printChildren(Object bs) {
         if(bs instanceof Bracket){
+            if(((Bracket) bs).cat.equals("V"))
+                inflect((String)((Bracket) bs).children[0]);
             if (((Bracket) bs).children != null){
-                Log.d(TAG, "CATEGORY:::::::::::: " + ((Bracket) bs).cat);
+                //Log.d(TAG, "CATEGORY:::::::::::: " + ((Bracket) bs).cat);
                 for(Object child : ((Bracket) bs).children){
                     printChildren(child);
                 }
             }
         }else if(bs instanceof String){
-            Log.d(TAG, (String) bs);
+            //Log.d(TAG, (String) bs);
         }
     }
 
@@ -56,6 +60,16 @@ public class MainActivity extends BaseActivity {
         Log.d(TAG, app.getFunction());
         for(Expr arg : app.getArguments()){
             Log.d(TAG, arg.toString());
+        }
+    }
+
+    private void inflect(String word) {
+        for(MorphoAnalysis an : eng.lookupMorpho(word)){
+            Expr e = Expr.readExpr(an.getLemma());
+            for(Map.Entry<String,String> entry  : swe.tabularLinearize(e).entrySet()) {
+                if(entry.getKey().contains("Pass"))
+                    Log.d(TAG, entry.getKey() + ": " + entry.getValue());
+            }
         }
     }
 
