f09753da2e3d6314a77211514c99f3ec25b0c00e
Isak Almeros <isak_almeros@hotmail.com>
2020-03-05 14:07:57 +0100

    Klickat ord skickas med till lexiconDetails, med hjälp av safe args

--- SmartLearning/app/src/main/java/org/grammaticalframework/View/Fragments/LexiconDetailsFragment.java
+++ SmartLearning/app/src/main/java/org/grammaticalframework/View/Fragments/LexiconDetailsFragment.java
@@ -5,7 +5,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -18,6 +21,7 @@ import org.grammaticalframework.ViewModel.LexiconWordAdapter;
 public class LexiconDetailsFragment extends BaseFragment {
 
     private ImageView backButton;
+    private String translatedWord;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -37,5 +41,17 @@ public class LexiconDetailsFragment extends BaseFragment {
 
         return fragmentView;
 
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        if(getArguments() != null){
+            LexiconDetailsFragmentArgs args = LexiconDetailsFragmentArgs.fromBundle(getArguments());
+            translatedWord = args.getMessage();
+            TextView word = view.findViewById(R.id.lexicon_details_word);
+            word.setText(translatedWord);
+        }
     }
 }
