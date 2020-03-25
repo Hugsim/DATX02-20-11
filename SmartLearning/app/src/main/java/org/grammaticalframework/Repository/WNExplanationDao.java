f6441a03c8613316a6dfcea15d2c5b80c2b0e119
htao <htao@student.chalmers.se>
2020-03-25 20:27:07 +0100

    Started with synonym

--- SmartLearning/app/src/main/java/org/grammaticalframework/Repository/WNExplanationDao.java
+++ SmartLearning/app/src/main/java/org/grammaticalframework/Repository/WNExplanationDao.java
@@ -32,4 +32,7 @@ public interface WNExplanationDao {
     @Query("SELECT * FROM WordNetExplanation_table WHERE function IN (:functions)")
     LiveData<List<WNExplanation>> getAllWordNetExplanations(List<String> functions);
 
+    @Query("SELECT * FROM WordNetExplanation_table WHERE synonym = :synonym")
+    WNExplanation getWNSynonym (String synonym);
+
 }
--- SmartLearning/app/src/main/java/org/grammaticalframework/Repository/WNExplanationRepository.java
+++ SmartLearning/app/src/main/java/org/grammaticalframework/Repository/WNExplanationRepository.java
@@ -44,4 +44,8 @@ public class WNExplanationRepository {
         return mWNExplanationDao.getWNExplanation(function);
     }
 
+    public WNExplanation getSynonym (String function){
+        return mWNExplanationDao.getWNSynonym(function);
+    }
+
 }
