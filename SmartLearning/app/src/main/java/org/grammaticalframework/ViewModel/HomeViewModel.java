554991f2b0d85c9698dca1a7be1852b72cd188a0
augustas <augustas@student.chalmers.se>
2020-04-08 15:58:06 +0200

    add language change in home page as well as trivia

--- SmartLearning/app/src/main/java/org/grammaticalframework/ViewModel/HomeViewModel.java
+++ SmartLearning/app/src/main/java/org/grammaticalframework/ViewModel/HomeViewModel.java
@@ -1,5 +1,44 @@
 package org.grammaticalframework.ViewModel;
 
-public class HomeViewModel extends BaseViewModel {
-    public HomeViewModel(){}
+import android.app.Application;
+
+import org.grammaticalframework.Language;
+import org.grammaticalframework.SmartLearning;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+public class HomeViewModel extends AndroidViewModel {
+    private SmartLearning sl;
+
+    public HomeViewModel(@NonNull Application application){
+        super(application);
+        sl = (SmartLearning) getApplication().getApplicationContext();
+    }
+
+    public List<Language> getAvailableLanguages() {
+        return sl.getAvailableLanguages();
+    }
+
+    public Language getSourceLanguage() {
+        return sl.getSourceLanguage();
+    }
+
+    public Language getTargetLanguage(){
+        return sl.getTargetLanguage();
+    }
+
+    public void setSourceLanguage(Language lang) {
+        sl.setSourceLanguage(lang);
+    }
+
+    public void setTargetLanguage(Language lang) {
+        sl.setTargetLanguage(lang);
+    }
+
+    public int getLanguageIndex(Language lang) {
+        return sl.getLanguageIndex(lang);
+    }
 }
